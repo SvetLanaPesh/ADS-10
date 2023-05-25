44db35cc5f7f426991cefa697e3cf5e18fa4fe21
SvetLanaPesh <124920308+SvetLanaPesh@users.noreply.github.com>
2023-05-25 08:27:47 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,69 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
+class Tree {
+ public:
+    struct Node {
+        char value;
+        int count;
+        Node** sons;
+    };
+ private:
+    Node* root = nullptr;
+    Node* node;
+    std::vector<std::vector<char>> rer;
+    void add(Node* node, std::vector<char> value) {
+        node->count = value.size();
+        if (node->count == 0) {
+            node->sons = nullptr;
+            return;
+        }
+        node->sons = new Node * [node->count];
+        for (int i = 0; i < node->count; i++) {
+            node->sons[i] = new Node;
+            node->sons[i]->value = value[i];
+        }
+        for (int i = 0; i < node->count; i+=1) {
+            std::vector<char> val(value);
+            auto it = std::remove(val.begin(), val.end(), node->sons[i]->value);
+            val.erase(it, val.end());
+            add(node->sons[i], val);
+        }
+    }
+    bool isLeaf(Node* node) {
+        return (node->sons == nullptr);
+    }
+    void treeTraversal(Node* node, std::vector<char>& path) {
+        if (node == nullptr) {
+            return;
+        }
+        path.push_back(node->value);
+        if (isLeaf(node)) {
+            rer.push_back(path);
+        }
+        for (int i = 0; i < node->count; i++) {
+            treeTraversal(node->sons[i], path);
+        }
+        path.pop_back();
+    }
+    void treeTraversal(Node* node) {
+        std::vector<char> path;
+        treeTraversal(node, path);
+    }
+ public:
+    Tree(std::vector<char> value) {
+        node = new Node;
+        root = node;
+        node->value = '*';
+        add(node, value);
+    }
+    void print() {
+        treeTraversal(root);
+    }
+    std::vector<std::vector<char>> Perm() {
+        return rer;
+    }
+};
 
 #endif  // INCLUDE_TREE_H_
